dea914a335e3aca945a92800d2c2154aec68b5c6
Scripted <62614570+Scriptedisles@users.noreply.github.com>
2022-05-26 14:19:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,4 +116,17 @@ client.on('interactionCreate', async interaction => {
 client.on('interactionCreate', interaction => {
 	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 });
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 keepAlive()
