484fd4a291d474da7e6a1e8bd4d8801c216fb022
ScriptedIsles <62614570+Scriptedisles@users.noreply.github.com>
2022-05-26 13:23:54 -0500

    Added handler

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,10 @@
-// will be used for deploying commands in a ltr update.
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require(process.env.TOKEN);
+const commands = [];
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
