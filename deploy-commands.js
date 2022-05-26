fc83eb4fe976182a937e00eb46454c65dea11b1b
Scripted <62614570+Scriptedisles@users.noreply.github.com>
2022-05-26 13:30:47 -0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,15 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token } = require(process.env.TOKEN);
+
 const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '9' }).setToken(token);
-
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
