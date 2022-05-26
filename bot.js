0de36eea07bfe42431425bf5b306c64762c72ad1
Scripted <62614570+Scriptedisles@users.noreply.github.com>
2022-05-26 13:26:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,4 +96,18 @@ client.on('interactionCreate', async interaction => {
 		interaction.editReply({ files: [file] });
 	}
 });
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 keepAlive()
