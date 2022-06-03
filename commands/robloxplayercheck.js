2e18bf49e248197add38488cc5bdc7cac0834fda
Scripted <62614570+Scriptedisles@users.noreply.github.com>
2022-06-03 18:36:01 +0000

    Pending changes exported from your codespace

--- commands/robloxplayercheck.js
+++ commands/robloxplayercheck.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 function getStatus() {
-    return fetch("https://api.roblox.com/users/")
+    return fetch(`https://api.roblox.com/users/${}`)
         .then(res => {
 
             return res.json()
@@ -21,6 +21,7 @@ module.exports = {
             option.setName('id')
                 .setDescription('RBLX id of the user your checking.')
                 .setRequired(true)),
+                
 async execute(interaction) {
         await getStatus().then(Status => interaction.reply(Status))
         
